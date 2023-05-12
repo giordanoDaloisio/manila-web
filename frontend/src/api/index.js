394253648211adcb5bd86052d559a6a6ccaa9e32
GIordano <giordano.daloisio@gmail.com>
2023-05-12 14:56:39 +0200

    changed endpoints

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,21 +1,25 @@
 import axios from "axios";
-
-const api = axios.create({
-  baseURL: "https://manila-sobigdata.d4science.org/",
-});
+// const api = axios.create({
+//   //baseURL: "https://manila-sobigdata.d4science.org/",
+//   baseURL: "http://localhost:5000/",
+// });
 
 export const generate = async (data) => {
-  const ris = await api.post("/generate", data, {
-    responseType: "blob",
-  });
+  const ris = await axios.post(
+    "https://manila-sobigdata.d4science.org/generate",
+    data,
+    {
+      responseType: "blob",
+    }
+  );
   return ris;
 };
 
 export const run = async (data, file) => {
   const values = { ...data, dataset: file };
-  const ris = await api.post("/run", values, {
+  const ris = await axios.post("/run", values, {
     headers: {
-      "Content-Type": "multipart/form-data",
+      "Content-Type": "application/json",
     },
   });
   return ris;
