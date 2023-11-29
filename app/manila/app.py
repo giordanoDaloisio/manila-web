7a0934efe49b49a571baf7b1a72d9b1aa479d862
giordano.daloisio <giordano.daloisio@gmail.com>
2023-11-29 15:35:17 +0100

    test

--- app/manila/app.py
+++ app/manila/app.py
@@ -27,35 +27,35 @@ class Homepage(Resource):
         return send_from_directory(app.static_folder, "index.html")
 
 
-class Generator(Resource):
-    def post(self):
-        params = request.get_json()
-        zip = generate_zip(params)
-        return send_file(zip, mimetype="zip", download_name="experiment.zip")
+# class Generator(Resource):
+#     def post(self):
+#         params = request.get_json()
+#         zip = generate_zip(params)
+#         return send_file(zip, mimetype="zip", download_name="experiment.zip")
 
 
-class Run(Resource):
-    def post(self):
-        params = request.form.to_dict()
-        params.update({"web": "web"})
-        data_extension = params.get("extension")
-        data = request.files["dataset"].read()
-        try:
-            folder_name = generate_code(params)
-            metrics, model = run_experiment(data, folder_name, data_extension)
-        except Exception as e:
-            app.logger.error(e.with_traceback(e.__traceback__).args)
-            message = json.dumps({"error": str(e)})
-            return Response(message, status=500, mimetype="application/json")
-        results = {"models": {}, "metrics": {}}
-        for k in metrics.keys():
-            if k == "fairness_method" or k == "model":
-                results["models"][k] = metrics[k]
-            else:
-                results["metrics"][k] = metrics[k]
-        response = make_response({"results": results, "model_path": model}, 200)
-        response.headers["Content-Type"] = "application/json"
-        return response
+# class Run(Resource):
+#     def post(self):
+#         params = request.form.to_dict()
+#         params.update({"web": "web"})
+#         data_extension = params.get("extension")
+#         data = request.files["dataset"].read()
+#         try:
+#             folder_name = generate_code(params)
+#             metrics, model = run_experiment(data, folder_name, data_extension)
+#         except Exception as e:
+#             app.logger.error(e.with_traceback(e.__traceback__).args)
+#             message = json.dumps({"error": str(e)})
+#             return Response(message, status=500, mimetype="application/json")
+#         results = {"models": {}, "metrics": {}}
+#         for k in metrics.keys():
+#             if k == "fairness_method" or k == "model":
+#                 results["models"][k] = metrics[k]
+#             else:
+#                 results["metrics"][k] = metrics[k]
+#         response = make_response({"results": results, "model_path": model}, 200)
+#         response.headers["Content-Type"] = "application/json"
+#         return response
 
 
 class KeepAlive(Resource):
@@ -69,8 +69,8 @@ class Model(Resource):
 
 
 api.add_resource(Homepage, "/")
-api.add_resource(Generator, "/generate")
-api.add_resource(Run, "/run")
+# api.add_resource(Generator, "/generate")
+# api.add_resource(Run, "/run")
 api.add_resource(KeepAlive, "/keepalive")
 api.add_resource(Model, "/model/<string:model_name>")
 
